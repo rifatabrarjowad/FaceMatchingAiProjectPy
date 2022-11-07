108bc64ca9f5ffc6bc3829b9cc230e1dfe770d1b
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-11-07 21:31:25 +0600

    file setup and static file setup

--- myApp/myApp/urls.py
+++ myApp/myApp/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import view
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view.home, name='home'),
+    path('', views.home, name='home'),
 ]

42eb3fa7d918b09fbf412ca1ec16e64baab8d578
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-11-07 22:38:45 +0600

    try to get img

--- myApp/myApp/views.py
+++ myApp/myApp/views.py
@@ -1,6 +1,22 @@
+import base64
 
 from django.shortcuts import render
-
+import numpy as np
+import cv2
+import face_recognition
 
 def home(request):
+    if request.method == 'POST':
+        inImg = request.FILES.get["inputImage"].read()
+
+        encoded = base64.b64encode(inImg)
+        mime = "image/jpg"
+        mime = mime + ";" if mime else ";"
+        input_image = "data:%sbase64,%s" % (mime, encoded)
+        print(input_image)
+
+
+        return render(request, "index.html", {{"input_image": input}})
+
+    ## do something with the images
     return render(request, 'index.html')
